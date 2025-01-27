c4db8f870fd91a8ab7bcd7af41a60566d387511b
Leon Biermann <biermann.leon@outlook.de>
2025-01-27 17:17:04 +0100

    reverting to old main

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
-import os
-import sys
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from db.database import engine
 from models.test import Base
 from routers import test_router, test_combination_router
+import os
 from fastapi.middleware.cors import CORSMiddleware
 
-# Initialize the database
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-# CORS configuration
 origins = ["http://localhost:3000"]
 
 app.add_middleware(
@@ -23,20 +20,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Determine the absolute path to the frontend build
-if getattr(sys, "frozen", False):
-    # If the application is run as a bundle (PyInstaller)
-    base_path = sys._MEIPASS
-else:
-    # If the application is run normally
-    base_path = os.path.dirname(os.path.abspath(__file__))
-
-frontend_build_dir = os.path.join(base_path, "frontend", "build")
-
-# Mount static files
-app.mount("/static", StaticFiles(directory=frontend_build_dir), name="static")
-
-# Include routers
+app.mount("/static", StaticFiles(directory="./frontend/build"), name="static")
 app.include_router(test_router.router)
 app.include_router(test_combination_router.router)
 
